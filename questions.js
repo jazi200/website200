7f110d6e51d7be6a565ad177356919ac7f93e269
jazi200 <95362234+jazi200@users.noreply.github.com>
2021-12-01 21:05:26 +0600

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,118 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the default value of the position property?",
+    answer: "static",
+    options: [
+      "relative",
+      "fixed",
+      "absolute",
+      "static"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How do you select an element with id 'demo'?",
+    answer: "#demo",
+    options: [
+      "*demo",
+      "#demo",
+      ".demo",
+      "demo"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How do you make each word in a text start with a capital letter?",
+    answer: "text-transform:capitalize",
+    options: [
+      "You can't do that with CSS",
+      "text-transform:capitalize",
+      "transform:capitalize",
+      "text-style:capitalize"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Which HTML attribute is used to define inline styles?",
+    answer: "style",
+    options: [
+      "style",
+      "font",
+      "class",
+      "styles"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Which is the correct CSS syntax?",
+    answer: "body {color: black;}",
+    options: [
+      "{body;color:black;}",
+      "body {color: black;}",
+      "{body:color=black;}",
+      "body:color=black;"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "How do you insert a comment in a CSS file?",
+    answer: "/* this is a comment */",
+    options: [
+      "// this is a comment",
+      "// this is a comment //",
+      "/* this is a comment */",
+      "' this is a comment"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Which CSS property controls the text size?",
+    answer: "font-size",
+    options: [
+      "font-style",
+      "text-size",
+      "text-style",
+      "font-size"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "How do you display hyperlinks without an underline?",
+    answer: "a {text-decoration:none;}",
+    options: [
+      "a {decoration:no-underline;}",
+      "a {text-decoration:none;}",
+      "a {underline:none;}",
+      "a {text-decoration:no-underline;}"
+    ]
+  },
+
+];
